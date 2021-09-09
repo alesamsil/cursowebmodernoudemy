4aa7bc53cc4781b9d9edaa0970a51db241ff2667
Alesamsil <alesamsil@gmail.com>
2021-09-09 20:35:55 -0300

    operadores

--- Exercicios-JS/fundamentos/15_operadores_destructuring.js
+++ Exercicios-JS/fundamentos/15_operadores_destructuring.js
@@ -50,3 +50,40 @@ recurso do ES2015
     console.log(nota);
     
 }
+
+/*
+Destructuring para paramentro de funcao
+recurso do ES2015
+*/
+{
+    function rand({min = 0, max = 1000}){
+        const valor = Math.random() * (max - mim) + min;
+        return Math.floor(valor);
+    }
+    const obj = {max: 50, min: 40}
+    console.log(rand(obj));
+    console.log(rand({min:955}));
+    console.log(rand({}));
+    //console.log(rand());
+}
+
+
+/*
+Destructuring dentro de parametros de função array
+recurso do ES2015
+*/
+
+{
+    function rand({min = 0, max = 1000}){
+        if (min > max) [min, max] = [max,min];
+        const valor = Math.random() * (max - min) + min;
+        return Math.floor(valor);
+    }
+    console.log(rand([50,40]));
+    console.log(rand([922]));
+    console.log(rand([, 10]));
+    console.log(rand([]));
+    //console.log(rand());
+
+
+}
