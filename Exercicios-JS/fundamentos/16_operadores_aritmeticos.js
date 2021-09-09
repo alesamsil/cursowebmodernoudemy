1105d4651f90ba12306466bea6a751bae0367aa5
Alesamsil <alesamsil@gmail.com>
2021-09-09 20:49:44 -0300

    operadores relacionais

---
+++ Exercicios-JS/fundamentos/16_operadores_aritmeticos.js
@@ -0,0 +1,14 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d;
+
+const subtracao = d - b;
+
+const multiplicacao = a * b;
+
+const divisao = d / a;
+
+//se o resto da divisão for zero é par se for 1 é ímpar
+const modulo = a % 2;
+
+console.log(soma,subtracao,multiplicacao,divisao,modulo);
