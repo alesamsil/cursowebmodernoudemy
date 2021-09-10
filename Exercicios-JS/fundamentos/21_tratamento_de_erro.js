707c54a0eec0e47ee288de9c496850ec5eab84f3
Alesamsil <alesamsil@gmail.com>
2021-09-10 14:19:28 -0300

    Tratamentos de erros

---
+++ Exercicios-JS/fundamentos/21_tratamento_de_erro.js
@@ -0,0 +1,28 @@
+function tratarErroELancar(erro){
+    //throw new Error('...');
+    //throw 10;
+    //throw true
+    //th 'mensagem'
+    throw{
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+
+function imprimirNomeGritado(obj) {
+    try{
+        console.log(obj.name.toUpperCase() + '!!!');
+    } 
+    catch (e) {
+        tratarErroELancar(e);
+    } 
+    finally {
+        console.log("final")
+    }
+}
+ //passando nome da erro e name funciona
+//const obj = {nome:'Roberto'};
+const obj = {name:'Roberto'};
+imprimirNomeGritado(obj);
