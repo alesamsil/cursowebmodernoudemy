ce4280fefe81e40965a8488131937fe61ed97e24
Alesamsil <alesamsil@gmail.com>
2021-09-09 21:52:40 -0300

    operadores ternarios

---
+++ Exercicios-JS/fundamentos/20_operadores_ternarios.js
@@ -0,0 +1,7 @@
+//primeira parte a expressão que retorna verdadeiro ou falso
+// expressão ? 'retorno verdadeiro' : 'retorno falso'
+
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado';
+
+console.log(resultado(7.1));
+console.log(resultado(6.1));
