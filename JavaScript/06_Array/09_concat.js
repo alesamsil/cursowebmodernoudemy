b29787f00aa612bc3f35932c4c8a9e868507f8e2
Alesamsil <alesamsil@gmail.com>
2021-09-17 18:02:49 -0300

    Concat

---
+++ JavaScript/06_Array/09_concat.js
@@ -0,0 +1,10 @@
+//Os arrays originais não são alterados
+const filhas = ['Valeskah', 'Cibalena'];
+const filhos = ['Uoxiton', 'Ueslei'];
+const todos = filhas.concat(filhos);
+console.log(todos, filhas, filhos);
+
+//concatena varios elementos
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]));
+
+
