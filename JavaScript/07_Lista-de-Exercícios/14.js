ac42cb41852e3ce8d81d719e7abba18d6002efe2
Alesamsil <alesamsil@gmail.com>
2021-09-17 23:22:52 -0300

    Exercicios

---
+++ JavaScript/07_Lista-de-Exercícios/14.js
@@ -0,0 +1,15 @@
+/* 
+14) Elabore uma função que receba um array de números e retorne um array que tenha todos os números que são
+pares e que também tenham índices pares.
+
+💡 Lembre-se que um número é par porque é divisível por 2, ou seja, o resto da divisão da divisão dele por
+2 é zero.
+
+Exemplos:
+receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]) // retornará []
+receberSomenteOsParesDeIndicesPares([10, 70, 22, 43]) // retornará [10, 22]
+*/
+
+
+receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]);
+receberSomenteOsParesDeIndicesPares([10, 70, 22, 43]);
