3947dbaa2e3ad2ee95ff0e8a2885bca460261437
Alesamsil <alesamsil@gmail.com>
2021-09-20 15:37:53 -0300

    Revisão

--- esnext/revisão.js
+++ esnext/revisão.js
@@ -44,3 +44,27 @@ function total(...numeros){
     return total
 }
 console.log(total(2, 3, 4, 5))
+
+//ES8: Object.values/Object.entries
+const obj = {a: 1, b:2, c:3}
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+//melhorias na Notação Literal
+const nome = 'Carla'
+const pessoa = {
+    nome,
+    ola(){
+        return 'Oi gente'
+    }
+}
+console.log(pessoa.nome, pessoa.ola())
+
+//class
+class Animal {}
+class Cachorro extends  Animal {
+    falar(){
+        return 'Au au'
+    }
+}
+console.log(new Cachorro().falar())
