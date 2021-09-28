735988a9a756cd7778f57158bca4f937e18a43f2
Alesamsil <alesamsil@gmail.com>
2021-09-27 22:44:27 -0300

    Formulario

--- HTML/Exercícios/form/server.js
+++ HTML/Exercícios/form/server.js
@@ -6,7 +6,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req,resp) =>{
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns, usuário incluido</h1>')
 })
 
+app.post('/usuarios/:id', (req,resp) =>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns, usuário alterado</h1>')
+})
 app.listen(3003)
